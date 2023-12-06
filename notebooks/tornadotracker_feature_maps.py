e284250da8bc93371ab51feaf44ca2116206fd6e
cam-alvarez <143116478+cam-alvarez@users.noreply.github.com>
2023-12-06 12:35:01 -0500

    Add files via upload

--- notebooks/tornadotracker_feature_maps.py
+++ notebooks/tornadotracker_feature_maps.py
@@ -105,6 +105,59 @@ for i in range(64):
 
 f0.shape,w0.shape
 
+
+
+import torch
+import matplotlib.pyplot as plt
+
+def plot_feature_maps_with_filters(feature_maps, filters):
+    # Remove batch dimension if it exists
+    if feature_maps.dim() == 4:
+        feature_maps = feature_maps.squeeze(0)
+
+    # Normalize feature maps to [0, 1]
+    feature_maps = (feature_maps - feature_maps.min()) / (feature_maps.max() - feature_maps.min())
+
+    def add_filter_to_feature_map(filter_tensor, feature_map_tensor):
+        # Ensure the feature map is 2D [H, W]
+        if feature_map_tensor.dim() > 2:
+            feature_map_tensor = feature_map_tensor.squeeze(0)
+
+        # Convert grayscale feature map to RGB by repeating the single channel 3 times
+        feature_map_rgb = feature_map_tensor.unsqueeze(0).repeat((3, 1, 1))
+
+        # Normalize the filter to [0, 1]
+        filter_tensor = (filter_tensor - filter_tensor.min()) / (filter_tensor.max() - filter_tensor.min())
+
+        # Ensure the filter fits into the feature map
+        min_dim = min(feature_map_tensor.shape)
+        filter_size = min(filter_tensor.shape[-1], min_dim)
+
+        # Crop the filter if needed
+        filter_cropped = filter_tensor[:, :filter_size, :filter_size]
+
+        # Overlay the RGB filter at the lower-left corner of the feature map
+        feature_map_rgb[:, -filter_size:, :filter_size] = filter_cropped
+
+        # Clip the values to be in the range [0, 1]
+        feature_map_rgb = torch.clamp(feature_map_rgb, 0, 1)
+
+        return feature_map_rgb
+
+    # Plot montage of feature maps
+    fig, axes = plt.subplots(4, 2, figsize=(15, 15))
+
+    for ax, feature_map, filter_ in zip(axes.flat, feature_maps, filters):
+        # Add RGB filter to grayscale feature map
+        modified_feature_map = add_filter_to_feature_map(filter_, feature_map)
+
+        # Plot modified feature map
+        ax.imshow(modified_feature_map.permute(1, 2, 0).cpu().numpy(), interpolation='none')  # Added 'none' interpolation
+        ax.axis('off')
+
+    plt.show()
+
+# @title
 plot_feature_maps_with_filters(f0, w0)
 
 
